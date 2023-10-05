8b55b90a41e3da3312da2ef9057b4336e6972db5
james hart <jamesmurphy197197@gmail.com>
2023-10-05 17:07:38 +0000

    [FEAT] Style header

--- client/heroestavern/src/components/header/header.js
+++ client/heroestavern/src/components/header/header.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./header.module.css"
 
 const Header = () => {
     return (
-        <nav>
-            <div>
-                logo
-            </div>
-            <div>
-                createpost
-            </div>
-            <div>
-                <ul>
-                    <li><NavLink to={"/"}>Home</NavLink></li>
-                    <li><NavLink to={"/characters"}>My Character Sheets</NavLink></li>
-                    <li><NavLink>Sign Out</NavLink></li>
-                </ul>
-            </div>
-        </nav>
+        <header>
+            <nav>
+                <div className={styles.logo}>
+                    logo
+                </div>
+                <div></div>
+                <div>
+                    <ul>
+                        <li><NavLink to={"/"}>Home</NavLink></li>
+                        <li><NavLink to={"/characters"}>My Character Sheets</NavLink></li>
+                        <li><NavLink>Sign Out</NavLink></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     )
 }
 
