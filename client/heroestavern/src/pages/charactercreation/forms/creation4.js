7ce7c6457c7e4c6c759be262beb7f7a16bbdbc7a
james hart <jamesmurphy197197@gmail.com>
2023-10-11 19:31:21 +0000

    [FEAT] Add state changes to creation4.js

--- client/heroestavern/src/pages/charactercreation/forms/creation4.js
+++ client/heroestavern/src/pages/charactercreation/forms/creation4.js
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
 import styles from "../charactercreation.module.css";
 
-const CreationForm4 = () => {
+const CreationForm4 = ({ characterState, setCharacterState }) => {
+  const handleAlliesChange = (e) => {
+    setCharacterState({...characterState, allies: e.currentTarget.value})
+  }
+  const handleEnemiesChange = (e) => {
+    setCharacterState({...characterState, enemies: e.currentTarget.value})
+  }
+  const handleFactionsChange = (e) => {
+    setCharacterState({...characterState, factions: e.currentTarget.value})
+  }
+  const handleBackstoryChange = (e) => {
+    setCharacterState({...characterState, backstory: e.currentTarget.value})
+  }
+  const handleNotesChange = (e) => {
+    setCharacterState({...characterState, notes: e.currentTarget.value})
+  }
   return (
     <form action="post" className={styles.descriptionForm}>
       <div>
@@ -11,6 +26,7 @@ const CreationForm4 = () => {
           cols="60"
           name="text"
           placeholder="Any allies you might have, such as party members or friends"
+          onChange={handleAlliesChange} value={characterState.allies}
         ></textarea>
       </div>
       <div>
@@ -20,6 +36,7 @@ const CreationForm4 = () => {
           cols="60"
           name="text"
           placeholder="Any enemies you might have, such as past encounters or leaders of enemy organizations"
+          onChange={handleEnemiesChange} value={characterState.enemies}
         ></textarea>
       </div>
       <div>
@@ -29,6 +46,7 @@ const CreationForm4 = () => {
           cols="60"
           name="text"
           placeholder="Any organizations you may be affiliated with, such as a guild or political party"
+          onChange={handleFactionsChange} value={characterState.factions}
         ></textarea>
       </div>
       <div>
@@ -38,6 +56,7 @@ const CreationForm4 = () => {
           cols="60"
           name="text"
           placeholder="The story of how you came to be the way you are"
+          onChange={handleBackstoryChange} value={characterState.backstory}
         ></textarea>
       </div>
       <div>
@@ -47,11 +66,9 @@ const CreationForm4 = () => {
           cols="60"
           name="text"
           placeholder="Any other notes on your character such as likes, dislikes, habits and behavioural patterns"
+          onChange={handleNotesChange} value={characterState.notes}
         ></textarea>
       </div>
-      {/* <div id={styles.SubmitInput}>
-        <input type="submit" className={styles.submitButton}/>
-      </div> */}
     </form>
   );
 };
