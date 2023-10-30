507eafbf24e1a8ae6c0b093fed4989124411ba5f
james hart <jamesmurphy197197@gmail.com>
2023-10-30 15:13:45 +0000

    [FEAT] Style and edit character list and character tiles

--- client/heroestavern/src/components/charactertile/charactertile.js
+++ client/heroestavern/src/components/charactertile/charactertile.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./charactertile.module.css";
 
-const CharacterTile = ({character}) => {
+const CharacterTile = ({ character }) => {
     const navigate = useNavigate();
     const handleClick = () => {
-        console.log(character)
-        navigate(`/characters/${character.id}`, {state:character})
-    }
+        console.log(character);
+        navigate(`/characters/${character.id}`, { state: character });
+    };
     return (
-        <button onClick={handleClick}>
-            {character.character_name}
-            {character.subclass.map(s=>s.subclass.name).join(" / ")}
-            {character.subrace.name}
-            {character.levels.map(l=>l.level).join("")}
+        <button onClick={handleClick} className={styles.characterTile}>
+            <h3>{character.character_name}</h3>
+            <div className={styles.statContainer}>
+                <div>{character.subrace.name}</div>
+                <div>{character.subclass.map((s) => s.subclass.name).join(" / ")}  {character.levels.map((c) => c.char_class.name).join(" / ")}</div>
+            </div>
+                <div className={styles.level}>{character.levels.map((l) => l.level).join("")}</div>
         </button>
-    )
-}
+    );
+};
 
-export default CharacterTile
+export default CharacterTile;
