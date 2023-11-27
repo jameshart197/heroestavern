ebdfb21f270b27850262c65c7d8b60e59d4b2b9f
james hart <jamesmurphy197197@gmail.com>
2023-11-27 20:33:22 +0000

    [FEAT] Add tab for allies, enemies and factions+orgs to character sheet

--- client/heroestavern/src/components/backstory/backstory.js
+++ client/heroestavern/src/components/backstory/backstory.js
@@ -1,13 +1,27 @@
 import React from "react";
-import styles from "./backstory.module.css"
+import styles from "./backstory.module.css";
+import { Tab } from "@headlessui/react";
 
 const Backstory = ({ character }) => {
-  return (
-      <div className={styles.backstoryContainer}>
-          <h4>Backstory</h4>
-          <p>{character.backstory}</p>
-      </div>
-  );
+    return (
+        <div className={styles.backstoryContainer}>
+          <h4>Description</h4>
+            <Tab.Group>
+                <Tab.List>
+                    <Tab>Backstory</Tab>
+                    <Tab>Allies</Tab>
+                    <Tab>Enemies</Tab>
+                    <Tab>Factions and Organizations</Tab>
+                </Tab.List>
+                <Tab.Panels>
+                    <Tab.Panel>{character.backstory}</Tab.Panel>
+                    <Tab.Panel>{character.allies}</Tab.Panel>
+                    <Tab.Panel>{character.enemies}</Tab.Panel>
+                    <Tab.Panel>{character.factions_and_orgs}</Tab.Panel>
+                </Tab.Panels>
+            </Tab.Group>
+        </div>
+    );
 };
 
 export default Backstory;
