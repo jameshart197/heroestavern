6c6cdf8a804a2d2a097d2e6eedfaee9dd9ffd817
james hart <jamesmurphy197197@gmail.com>
2023-11-30 21:39:51 +0000

    [STYLE] Style for resposiveness across the board

--- client/heroestavern/src/components/infopanel/infopanel.js
+++ client/heroestavern/src/components/infopanel/infopanel.js
@@ -6,44 +6,46 @@ const Infopanel = ({ character }) => {
     return (
         <div className={styles.topbar}>
             <button className={styles.editCharButton}>
-                <NavLink to="/charactercreation" state={character}>Edit</NavLink>
+                <NavLink to="/charactercreation" state={character}>
+                    Edit
+                </NavLink>
             </button>
             <div>
                 <div className={styles.label}>Name</div>
+                <div className={styles.response}>{character.character_name}</div>
             </div>
             <div>
                 <div className={styles.label}>Race</div>
+                <div className={styles.response}>{character.subrace.name}</div>
             </div>
             <div>
                 <div className={styles.label}>Class</div>
+                <div className={styles.response}>
+                    {character.levels.map(
+                        (l, idx) =>
+                            l.char_class.name +
+                            " - " +
+                            character.subclass.find((sc) => sc.subclass.parent_class === l.char_class.id).subclass.name +
+                            (idx + 1 < character.levels.length ? " / " : "")
+                    )}
+                </div>
             </div>
             <div>
                 <div className={styles.label}>Level</div>
+                <div className={styles.response}>{character.levels.reduce((a, b) => a + b.level, 0)}</div>
             </div>
             <div>
                 <div className={styles.label}>Inspiration</div>
+                <div className={styles.response}>
+                    <input type="checkbox" id="Inspiration" name="Inspiration" checked={character.inspiration} />
+                    <label htmlFor="Inspiration"></label>
+                </div>
             </div>
             <div>
                 <div className={styles.label}>HP</div>
-            </div>
-            <div className={styles.response}>{character.character_name}</div>
-            <div className={styles.response}>{character.subrace.name}</div>
-            <div className={styles.response}>
-                {character.levels.map(
-                    (l, idx) =>
-                        l.char_class.name +
-                        " - " +
-                        character.subclass.find((sc) => sc.subclass.parent_class === l.char_class.id).subclass.name +
-                        (idx + 1 < character.levels.length ? " / " : "")
-                )}
-            </div>
-            <div className={styles.response}>{character.levels.reduce((a, b) => a + b.level, 0)}</div>
-            <div className={styles.response}>
-                <input type="checkbox" id="Inspiration" name="Inspiration" checked={character.inspiration} />
-                <label htmlFor="Inspiration"></label>
-            </div>
-            <div className={styles.hp}>
-                {character.hit_points} / {character.hit_points}
+                <div className={styles.hp}>
+                    {character.hit_points} / {character.hit_points}
+                </div>
             </div>
         </div>
     );
--- client/heroestavern/src/components/passives/passives.js
+++ client/heroestavern/src/components/passives/passives.js
@@ -4,45 +4,60 @@ import Scorebox from "../scorebox/scorebox";
 import { modifierCalculator, skillProfLevel } from "../../helpers/modifier";
 
 const Passives = ({ character, profBonus }) => {
-  const wisdom = character.attributes.find(a=>a.attribute.id===5)
-  const intelligence = character.attributes.find(a=>a.attribute.id===4)
-  const perception = character.skills.find(s=>s.skill.name==="Perception")
-  const investigation = character.skills.find(s=>s.skill.name==="Investigation")
-  const insight = character.skills.find(s=>s.skill.name==="Insight")
-  const perceptionLevel = perception?skillProfLevel(perception, character):undefined
-  const investigationLevel = investigation?skillProfLevel(investigation, character):undefined
-  const insightLevel = insight?skillProfLevel(insight, character):undefined
-  const perceptionMultiplier = perceptionLevel===0?1:perceptionLevel===1?2:perceptionLevel===2?0.5:0
-  const investigationMultiplier = investigationLevel===0?1:investigationLevel===1?2:investigationLevel===2?0.5:0
-  const insightMultiplier = insightLevel===0?1:insightLevel===1?2:insightLevel===2?0.5:0
-  return (
-    <div>
-      <ul className={styles.passives}>
-        <li>
-          Passive Perception:{" "}
-          <div>
-            <Scorebox value={(+modifierCalculator(wisdom, profBonus*perceptionMultiplier)+10)}></Scorebox>
-          </div>
-        </li>
-        <li>
-          Passive Investigation:{" "}
-          <div>
-            <Scorebox value={(+modifierCalculator(intelligence, profBonus*investigationMultiplier)+10)}></Scorebox>
-          </div>
-        </li>
-        <li>
-          Passive Insight:{" "}
-          <div>
-            <Scorebox value={(+modifierCalculator(wisdom, profBonus*insightMultiplier)+10)}></Scorebox>
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
+    const wisdom = character.attributes.find((a) => a.attribute.id === 5);
+    const intelligence = character.attributes.find((a) => a.attribute.id === 4);
+    const perception = character.skills.find((s) => s.skill.name === "Perception");
+    const investigation = character.skills.find((s) => s.skill.name === "Investigation");
+    const insight = character.skills.find((s) => s.skill.name === "Insight");
+    const perceptionLevel = perception ? skillProfLevel(perception, character) : undefined;
+    const investigationLevel = investigation ? skillProfLevel(investigation, character) : undefined;
+    const insightLevel = insight ? skillProfLevel(insight, character) : undefined;
+    const perceptionMultiplier = perceptionLevel === 0 ? 1 : perceptionLevel === 1 ? 2 : perceptionLevel === 2 ? 0.5 : 0;
+    const investigationMultiplier = investigationLevel === 0 ? 1 : investigationLevel === 1 ? 2 : investigationLevel === 2 ? 0.5 : 0;
+    const insightMultiplier = insightLevel === 0 ? 1 : insightLevel === 1 ? 2 : insightLevel === 2 ? 0.5 : 0;
+    return (
+        // <div>
+        //   <ul className={styles.passives}>
+        //     <li>
+        //       Passive Perception:{" "}
+        //       <div>
+        //         <Scorebox value={(+modifierCalculator(wisdom, profBonus*perceptionMultiplier)+10)}></Scorebox>
+        //       </div>
+        //     </li>
+        //     <li>
+        //       Passive Investigation:{" "}
+        //       <div>
+        //         <Scorebox value={(+modifierCalculator(intelligence, profBonus*investigationMultiplier)+10)}></Scorebox>
+        //       </div>
+        //     </li>
+        //     <li>
+        //       Passive Insight:{" "}
+        //       <div>
+        //         <Scorebox value={(+modifierCalculator(wisdom, profBonus*insightMultiplier)+10)}></Scorebox>
+        //       </div>
+
+        <div className={styles.passives}>
+            Passive Perception:{" "}
+            <div>
+                <Scorebox value={+modifierCalculator(wisdom, profBonus * perceptionMultiplier) + 10}></Scorebox>
+            </div>
+            Passive Investigation:{" "}
+            <div>
+                <Scorebox value={+modifierCalculator(intelligence, profBonus * investigationMultiplier) + 10}></Scorebox>
+            </div>
+            Passive Insight:{" "}
+            <div>
+                <Scorebox value={+modifierCalculator(wisdom, profBonus * insightMultiplier) + 10}></Scorebox>
+            </div>
+            {/* </div> */}
+            {/* </li>
+      </ul> */}
+        </div>
+    );
 };
 
 export default Passives;
 
-// scorebox values should =  10 + modifiercalculator(attribute, proflevel) 
-// where attribute= wisdom, intelligence, wisdom and 
+// scorebox values should =  10 + modifiercalculator(attribute, proflevel)
+// where attribute= wisdom, intelligence, wisdom and
 // proflevel = character.skills.proficiencylevel in correlating skills (find skill.name === character.skills.name?)
