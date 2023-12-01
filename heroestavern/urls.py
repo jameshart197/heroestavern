d398dac2dd7521f17cf94b11ee564ab18fec6edf
james hart <jamesmurphy197197@gmail.com>
2023-12-01 18:17:35 +0000

    [FIX] Correct hard-coded URL and revert api/dj-rest-auth logic

--- heroestavern/urls.py
+++ heroestavern/urls.py
@@ -6,20 +6,10 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/', include ('api.urls', namespace='api')),
-    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
-        'api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
 ]
 
 handler404 = TemplateView.as_view(template_name='index.html')
-
-# urlpatterns = [
-#     path('', TemplateView.as_view(template_name='index.html')),
-#     path('admin/', admin.site.urls),
-#     path('api/', include ('api.urls', namespace='api')),
-#     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-#     path(
-#         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
-#     ),
-# ]
